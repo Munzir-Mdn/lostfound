36945ae0a3a3dd9a974fd6d655621dcec711b2cf
Munzir-Mdn <munzir.mdn@gmail.com>
2025-05-24 20:13:10 +0800

    Fix MySQL Railway connection

--- db_config.php
+++ db_config.php
@@ -1,11 +1,12 @@
 <?php
 $host = getenv("MYSQLHOST");
 $user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
 $port = getenv("MYSQLPORT");
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
+$conn = new mysqli($host, $user, $password, $database, $port);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
