eaaed4b1afe8b17c2c19b6cde2787f646ea70d5b
Munzir-Mdn <munzir.mdn@gmail.com>
2025-05-24 23:08:54 +0800

    update db_config.php to test railway connection

--- backend/db_config.php
+++ backend/db_config.php
@@ -1,13 +1,13 @@
 <?php
-$host = getenv("MYSQLHOST");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
+$host = "mysql.railway.internal";   // contoh host dari Railway
+$user = "root";                                  // user dari Railway
+$pass = "UIZmbWXwMOeoOSoatLBVhMHLoswvOYbS";             // password dari Railway
+$db   = "railway";                               // nama database dari Railway
+$port = 3306;                                     // port dari Railway
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
-// Check connection
+// Semak sambungan
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
